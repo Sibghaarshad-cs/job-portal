10b09b95b563b6dc25ef521cab2a292cacfd2e72
Sibgha Arshad <sibghaarshad16@gmail.com>
2026-08-06 22:14:42 +0500

    Remove unused CTA component from Home page

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import Navbar from "../components/landing/Navbar/navbar";
 import Hero from "../components/landing/Hero/Hero";
 import Features from "../components/landing/Features/Features";
 import FeaturedJobs from "../components/landing/FeaturedJobs/FeaturedJobs";
-import EmployerBanner from "../components/common/EmployerBanner"; 
+import EmployerBanner from "../components/common/EmployerBanner";
 import Statistics from "../components/landing/Statistics/Statistics";
-import CTA from "../components/landing/CTA/CTA";
 import Footer from "../components/landing/Footer/Footer";
 export default function Home() {
   return (
@@ -18,7 +17,6 @@ export default function Home() {
 
       <HowItWorks />
       <Statistics />
-      <CTA />
 
       <Footer />
     </>
